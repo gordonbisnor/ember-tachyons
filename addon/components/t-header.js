fa051f854be8cc15cb6d50ab2fd1ae73f6f33dbe
John Otander <johnotander@gmail.com>
2016-04-20 13:28:30 -0600

    Time to revisit the drawing boards

--- addon/components/t-header.js
+++ addon/components/t-header.js
@@ -1,13 +1,7 @@
 import Ember from 'ember';
-import Tachyons from '../mixins/tachyons';
 import layout from '../templates/components/t-header';
 
-export default Ember.Component.extend(Tachyons, {
-  defaultSpacing: {
-    padding: 3,
-    'padding-ns': 4
-  },
-
+export default Ember.Component.extend({
   tagName: 'header',
-  layout,
+  layout
 });
--- addon/mixins/tachyons.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-import TachyonsFontSize from './tachyons-font-size';
-import TachyonsFontWeight from './tachyons-font-weight';
-import TachyonsOpacity from './tachyons-opacity';
-import TachyonsSpacing from './tachyons-spacing';
-
-export default Ember.Mixin.create(TachyonsFontSize, TachyonsFontWeight,
-  TachyonsOpacity, TachyonsSpacing, {
-});

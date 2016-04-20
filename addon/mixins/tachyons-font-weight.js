750b50be4a899bcc0ba9c64aa87a6d8564dd6190
John Otander <johnotander@gmail.com>
2016-04-20 08:44:58 -0600

    Add font weight support

--- addon/mixins/tachyons-font-weight.js
+++ addon/mixins/tachyons-font-weight.js
@@ -1,4 +1,64 @@
 import Ember from 'ember';
 
 export default Ember.Mixin.create({
+  fontWeight: null,
+  'fontWeight-ns': null,
+  'fontWeight-m': null,
+  'fontWeight-l': null,
+
+  classNameBindings: [
+    'fontWeightCx'
+  ],
+
+  fontWeightCx: Ember.computed('fontWeight', 'fontWeight-ns', 'fontWeight-m', 'fontWeight-l', function() {
+    const cx = [];
+
+    const f = this.get('fontWeight');
+    const fNs = this.get('fontWeight-ns');
+    const fM = this.get('fontWeight-m');
+    const fL = this.get('fontWeight-l');
+
+    if (Ember.isPresent(f) && isKnownFontWeightValue(f)) { cx.push(getFontWeightClass(f)); }
+    if (Ember.isPresent(fNs) && isKnownFontWeightValue(fNs)) { cx.push(getFontWeightClass(fNs, 'ns')); }
+    if (Ember.isPresent(fM) && isKnownFontWeightValue(fM)) { cx.push(getFontWeightClass(fM, 'm')); }
+    if (Ember.isPresent(fL) && isKnownFontWeightValue(fL)) { cx.push(getFontWeightClass(fL, 'l')); }
+
+    return cx.join(' ');
+  })
 });
+
+function getFontWeightClass(value, mq) {
+  let klass = 'fw';
+
+  if (value === 'bold') {
+    klass = 'b';
+  } else if(value === 'normal') {
+    klass = 'normal';
+  } else {
+    klass += value;
+  }
+
+  if (Ember.isPresent(mq)) {
+    klass += '-' + mq;
+  }
+
+  return klass;
+}
+
+function isKnownFontWeightValue(value) {
+  const knownValues = {
+    1: true,
+    2: true,
+    3: true,
+    4: true,
+    5: true,
+    6: true,
+    7: true,
+    8: true,
+    9: true,
+    bold: true,
+    normal: true
+  };
+
+  return Ember.isPresent(value) && knownValues[value];
+}
--- tests/unit/mixins/tachyons-font-weight-test.js
+++ tests/unit/mixins/tachyons-font-weight-test.js
@@ -4,9 +4,23 @@ import { module, test } from 'qunit';
 
 module('Unit | Mixin | tachyons font weight');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
+test('binds attrs to classes', function(assert) {
   let TachyonsFontWeightObject = Ember.Object.extend(TachyonsFontWeightMixin);
   let subject = TachyonsFontWeightObject.create();
-  assert.ok(subject);
+
+  subject.set('fontWeight', 3);
+  subject.set('fontWeight-l', 9);
+  subject.set('fontWeight-ns', 'bold');
+
+  assert.equal(subject.get('fontWeightCx'), 'fw3 b-ns fw9-l');
+});
+
+test('ignores unknown values', function(assert) {
+  let TachyonsFontWeightObject = Ember.Object.extend(TachyonsFontWeightMixin);
+  let subject = TachyonsFontWeightObject.create();
+
+  subject.set('fontWeight-l', 10);
+  subject.set('fontWeight-ns', 'bld');
+
+  assert.equal(subject.get('fontWeightCx'), '');
 });
